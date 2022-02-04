862ba35e9fe05d9e9080b497012eeeeb7f91fc8e
Daniel Vinícius <viniccius774@gmail.com>
2022-02-04 17:08:19 -0300

    Centralizando árvore

--- exercicios-lp2/3.c
+++ exercicios-lp2/3.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 main()
 {
-    int linhas, cont, totalCaracteresPorLinha;
-    cont = 0;
+    int linhas, contador, totalCaracteresPorLinha, tamanhoCentralizado;
+    contador = 0;
     totalCaracteresPorLinha = 1;
     printf("Quantas linhas serao impressas?\n");
     scanf("%d", &linhas);
-    while (cont < linhas)
+    while (contador < linhas)
     {
+        tamanhoCentralizado = (linhas + (linhas - 1) - totalCaracteresPorLinha) / 2;
+        /*pega o total de caracteres da ultima linha para saber o tamanho maximo
+        da arvore, subtrai um e divide por dois para pegar a metade*/
+
+        for (int j = 0; j < tamanhoCentralizado; j++)
+        {
+            printf(" ");
+        }
         for (int i = 0; i < totalCaracteresPorLinha; i++)
             printf("*");
         totalCaracteresPorLinha += 2;
         printf("\n");
-        cont++;
+        contador++;
     }
     return 0;
 }
