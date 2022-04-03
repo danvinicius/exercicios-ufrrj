0ea3920c24e38dd97b931e13b728237bea38fedc
Daniel Vinícius <viniccius774@gmail.com>
2022-04-03 15:06:28 -0300

    add turtle draw exercise in python

---
+++ monitoria/turtle_draw.py
@@ -0,0 +1,544 @@
+import turtle
+
+palavra = input("Digite uma palavra: ")
+
+bob = turtle.Turtle()
+bob.shape("turtle")
+bob.speed(5)
+bob.pensize(4)
+bob.penup()
+bob.left(180)
+bob.forward(450)
+bob.left(180)
+bob.pendown()
+
+
+def a():
+    bob.pendown()
+    bob.left(75.96)
+    bob.forward(103.077)
+    bob.right(75.96 * 2)
+    bob.forward(103.077 / 2)
+    bob.left(75.96 + 180)
+    bob.forward(12.46 * 2)
+    bob.left(180)
+    bob.forward(12.46 * 2)
+    bob.right(75.96)
+    bob.forward(103.077 / 2)
+    bob.left(75.96)
+    bob.penup()
+    bob.forward(20)
+
+
+def b():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(35)
+    bob.right(45)
+    bob.forward(21.21)
+    bob.right(45)
+    bob.forward(27.5)
+    bob.right(60)
+    bob.forward(15)
+    bob.left(120)
+    bob.forward(15)
+    bob.right(60)
+    bob.forward(27.5)
+    bob.right(45)
+    bob.forward(21.21)
+    bob.right(45)
+    bob.forward(35)
+    bob.left(180)
+    bob.penup()
+    bob.forward(70)
+
+
+def c():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.left(180)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(50)
+    bob.penup()
+    bob.forward(20)
+
+
+def d():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(35)
+    bob.right(45)
+    bob.forward(21.21)
+    bob.right(45)
+    bob.forward(70)
+    bob.right(45)
+    bob.forward(21.21)
+    bob.right(45)
+    bob.forward(35)
+    bob.left(180)
+    bob.penup()
+    bob.forward(70)
+
+
+def e():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(180)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.right(180)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.penup()
+    bob.forward(20)
+
+
+def f():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(180)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.right(180)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.penup()
+    bob.forward(70)
+
+
+def g():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.left(180)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(35)
+    bob.left(180)
+    bob.forward(35)
+    bob.right(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.penup()
+    bob.forward(20)
+
+
+def h():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(180)
+    bob.forward(100)
+    bob.left(90)
+    bob.penup()
+    bob.forward(20)
+
+
+def i():
+    bob.pendown()
+    bob.forward(25)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(25)
+    bob.left(180)
+    bob.forward(50)
+    bob.left(180)
+    bob.forward(25)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(25)
+    bob.penup()
+    bob.forward(20)
+
+
+def j():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(30)
+    bob.left(180)
+    bob.forward(30)
+    bob.left(90)
+    bob.forward(40)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(40)
+    bob.left(180)
+    bob.forward(50)
+    bob.left(180)
+    bob.forward(10)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.penup()
+    bob.forward(30)
+
+
+def k():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(50)
+    bob.left(135)
+    bob.forward(70.71)
+    bob.left(180)
+    bob.forward(70.71)
+    bob.left(90)
+    bob.forward(70.71)
+    bob.left(45)
+    bob.penup()
+    bob.forward(20)
+
+
+def l():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(50)
+    bob.penup()
+    bob.forward(20)
+
+
+def m():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90 + 67.50)
+    bob.forward(55.9)
+    bob.left(2 * 67.50)
+    bob.forward(55.9)
+    bob.right(67.50 + 90)
+    bob.forward(100)
+    bob.left(90)
+    bob.penup()
+    bob.forward(20)
+
+
+def n():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90 + 63.43)
+    bob.forward(111.80)
+    bob.left(63.43 + 90)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(100)
+    bob.left(90)
+    bob.penup()
+    bob.forward(20)
+
+
+def o():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(180)
+    bob.forward(50)
+    bob.penup()
+    bob.forward(20)
+
+
+def p():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(90)
+    bob.forward(40)
+    bob.right(90)
+    bob.forward(50)
+    bob.penup()
+    bob.right(180)
+    bob.forward(50)
+    bob.right(90)
+    bob.forward(60)
+    bob.left(90)
+    bob.forward(20)
+
+
+def q():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(35)
+    bob.right(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(35)
+    bob.left(180 + 55)
+    bob.forward(30.51)
+    bob.right(2 * 55)
+    bob.forward(30.51)
+    bob.left(55)
+    bob.forward(15)
+    bob.penup()
+    bob.forward(20)
+
+
+def r():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(90)
+    bob.forward(50)
+    bob.left(90 + 45)
+    bob.forward(70.71)
+    bob.left(45)
+    bob.penup()
+    bob.forward(20)
+
+
+def s():
+    bob.pendown()
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(50)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(90)
+    bob.forward(50)
+    bob.right(90)
+    bob.penup()
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(20)
+
+
+def t():
+    bob.penup()
+    bob.forward(25)
+    bob.left(90)
+    bob.pendown()
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(25)
+    bob.left(180)
+    bob.forward(50)
+    bob.left(180)
+    bob.forward(25)
+    bob.left(90)
+    bob.penup()
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(55)
+
+
+def u():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(50)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(100)
+    bob.left(90)
+    bob.penup()
+    bob.forward(20)
+
+
+def v():
+    bob.left(90)
+    bob.penup()
+    bob.forward(100)
+    bob.right(90 + 78.75)
+    bob.pendown()
+    bob.forward(103.077)
+    bob.left(2 * 78.75)
+    bob.forward(103.077)
+    bob.right(78.75 + 90)
+    bob.penup()
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(20)
+
+
+def w():
+    bob.pendown()
+    bob.left(90)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(100)
+    bob.left(90 + 67.5)
+    bob.forward(55.9)
+    bob.right(2 * 67.50)
+    bob.forward(55.9)
+    bob.left(90 + 67.50)
+    bob.forward(100)
+    bob.left(180)
+    bob.forward(100)
+    bob.left(90)
+    bob.penup()
+    bob.forward(20)
+
+
+def x():
+    bob.pendown()
+    bob.left(63.5)
+    bob.forward(111.8)
+    bob.left(180)
+    bob.forward(111.8 / 2)
+    bob.right(63.5 * 2)
+    bob.forward(111.8 / 2)
+    bob.right(180)
+    bob.forward(111.8)
+    bob.left(63.5)
+    bob.penup()
+    bob.forward(20)
+
+
+def y():
+    bob.penup()
+    bob.forward(25)
+    bob.left(90)
+    bob.pendown()
+    bob.forward(50)
+    bob.left(26.57)
+    bob.forward(55.9)
+    bob.left(180)
+    bob.forward(55.9)
+    bob.left(2 * 63.43)
+    bob.forward(55.9)
+    bob.left(180)
+    bob.forward(55.9)
+    bob.left(26.57)
+    bob.forward(50)
+    bob.left(90)
+    bob.penup()
+    bob.forward(45)
+
+
+def z():
+    bob.pendown()
+    bob.left(63.5)
+    bob.forward(111.8)
+    bob.left(180 - 63.5)
+    bob.forward(50)
+    bob.left(90)
+    bob.penup()
+    bob.forward(100)
+    bob.left(90)
+    bob.pendown()
+    bob.forward(50)
+    bob.penup()
+    bob.forward(20)
+
+
+for letra in palavra:
+    if letra == "a":
+        a()
+    elif letra == "b":
+        b()
+    elif letra == "c":
+        c()
+    elif letra == "d":
+        d()
+    elif letra == "e":
+        e()
+    elif letra == "f":
+        f()
+    elif letra == "g":
+        g()
+    elif letra == "h":
+        h()
+    elif letra == "i":
+        i()
+    elif letra == "j":
+        j()
+    elif letra == "k":
+        k()
+    elif letra == "l":
+        l()
+    elif letra == "m":
+        m()
+    elif letra == "n":
+        n()
+    elif letra == "o":
+        o()
+    elif letra == "p":
+        p()
+    elif letra == "q":
+        q()
+    elif letra == "r":
+        r()
+    elif letra == "s":
+        s()
+    elif letra == "t":
+        t()
+    elif letra == "u":
+        u()
+    elif letra == "v":
+        v()
+    elif letra == "w":
+        w()
+    elif letra == "x":
+        x()
+    elif letra == "y":
+        y()
+    elif letra == "z":
+        z()
